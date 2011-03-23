d0ad3ee2c5d16efe6d003404b4f39bca2233be4f
Frank <frankkwok@gmail.com>
2011-03-23 18:43:48 -0400

    Starting Rails-flavored Ruby

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,5 +24,36 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+      :content => "Ruby on Rails Tutorial Sample App | Home")
+  end
+
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+      :content => "Ruby on Rails Tutorial Sample App | Contact")
+  end
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+      :content => "Ruby on Rails Tutorial Sample App | About")
+  end
+
+  it "should have the right title" do
+    get 'help'
+    response.should have_selector("title",
+      :content => "Ruby on Rails Tutorial Sample App | Help")
+  end
+
 end
 
