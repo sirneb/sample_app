8ca44a3882e4db67bfee7d4c68a9208855bfcce4
Frank <frankkwok@gmail.com>
2011-03-23 23:31:01 -0400

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -19,10 +19,15 @@ describe "LayoutLinks" do
 
   end
 
-  it "should have a Home page at '/help'" do
+  it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
 
+  end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title',:content => "Sign up")
   end
 
 end
