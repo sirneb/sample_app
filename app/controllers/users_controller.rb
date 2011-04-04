71218f3643152fd1c82c33f09d876887698f60f7
Frank <frankkwok@gmail.com>
2011-04-04 17:59:51 -0400

    have users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
 
   def show
@@ -50,6 +50,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
   private 
   
     def authenticate
@@ -60,5 +70,6 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,7 @@ module SessionsHelper
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
   end
 
@@ -30,8 +31,17 @@ module SessionsHelper
     user == current_user
   end
 
-  def deny_access
-    redirect_to signin_path, :notice => "Please sign in to access this page."
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
   end
 
   private
